92cec0e82424eb5272ea6a6cd60bed6fd8b7b660
brdavis330 _ <brandimarie4259@gmail.com>
2023-06-15 19:49:21 +0000

    migrate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  coments_count   :integer
+#  email           :string
+#  likes_count     :integer
+#  password_digest :string
+#  ptivate         :boolean
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
